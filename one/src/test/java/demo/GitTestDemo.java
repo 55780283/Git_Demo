4f3da4705e28012e9ac7b063963367f449b2183c
liumingwei <55780283@qq.com>
2020-03-08 13:43:42 +0800

    ceshi

--- one/src/test/java/demo/GitTestDemo.java
+++ one/src/test/java/demo/GitTestDemo.java
@@ -2,6 +2,6 @@ package demo;
 
 public class GitTestDemo {
     public static void main(String[] args) {
-        System.out.println("Git Demo 测试");
+        System.out.println("Git Demo 测试  ");
     }
 }
